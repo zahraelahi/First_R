c82dea9a215607e6ff380ae2c13a8c60c9871ce9
Zahra <zahra.elaahi@gmail.com>
2020-05-12 18:13:07 +1000

    practice some functions

--- first.r
+++ first.r
@@ -1,1 +1,14 @@
 1+1
+w<-1:3
+names(w)<-c('a','b','c')
+w
+"http://www.jaredlander.com/data/Tomato%20First.csv"
+tomato<- read.csv(file="http://www.jaredlander.com/data/Tomato%20First.csv",header=TRUE,sep=',')
+head(tomato)
+class(tomato$Tomato)
+tomato<- read.csv(file="http://www.jaredlander.com/data/Tomato%20First.csv",header=TRUE,sep=',',stringsAsFactors = FALSE)
+class(tomato$Tomato)
+hist(tomato$Price)
+ggplot2(data=tomato)
+library(ggplot2)
+ggplot(data=tomato)+geom_histogram(aes(x=Price),fill=FALSE)
